f01a6a753b8eb0c5ce0e3c7ded524f0754ad816e
Ali Aljoubory <aaljoubory@gmail.com>
2020-04-15 21:01:57 +0100

    Initial Commit

---
+++ GitHub Followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Followers
+//
+//  Created by Ali Aljoubory on 15/04/2020.
+//  Copyright © 2020 Ali Aljoubory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
